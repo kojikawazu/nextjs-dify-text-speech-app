0eef7b6c360c348d53ad8e4ae7b7d1671c582b6d
kojikawazu <nanairo7.daydream21@gmail.com>
2024-11-20 08:15:41 +0900

    API整理

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -13,15 +13,20 @@ export const dynamic = 'force-dynamic';
 export async function GET() {
     console.log('GET ', COMMON_CONSTANTS.URL.API_AUTH_SESSION);
 
-    // ルートハンドラー用のSupabaseクライアントの作成
-    const supabase = createRouteHandlerClient({ cookies });
+    try {
+        // ルートハンドラー用のSupabaseクライアントの作成
+        const supabase = createRouteHandlerClient({ cookies });
 
-    // セッションの取得
-    const {
-        data: { session },
-    } = await supabase.auth.getSession();
+        // セッションの取得
+        const {
+            data: { session },
+        } = await supabase.auth.getSession();
 
-    // セッションをJSONで返す
-    console.log('GET ', COMMON_CONSTANTS.URL.API_AUTH_SESSION, 'response', session);
-    return NextResponse.json(session);
+        // セッションをJSONで返す
+        console.log('GET ', COMMON_CONSTANTS.URL.API_AUTH_SESSION, 'response', session);
+        return NextResponse.json(session);
+    } catch (error) {
+        console.error('GET ', COMMON_CONSTANTS.URL.API_AUTH_SESSION, 'error', error);
+        return NextResponse.json({ error: 'Failed to fetch session' }, { status: 500 });
+    }
 }
